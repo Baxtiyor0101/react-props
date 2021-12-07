6d6d9474ee4c5b2c75ccb71bb9ead8d5657080c5
Baxtiyor <oqonboyevb@gmail.com>
2021-12-07 22:25:12 +0500

    counter

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,95 @@
-import React from "react"; 
+import React from 'react';
 
-class Student extends React.Component{
-  render(){
-    return(
-      <div>
-        <h1>Hi {this.props.status}</h1>
-        {this.props.children}
-        <button>See more</button>
-      </div>
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count:0,
+      name:'',
+      data:{
+        status:'OK',
+        dataList: [
+          {
+            id:1,
+            name:'Eshmat',
+            status:'Mentor',
+          },
+          {
+            id:2,
+            name:'Toshmat',
+            status:'Gentor',
+          },
+          {
+            id:3,
+            name:'Gulmat',
+            status:'Waiter',
+          },
+          {
+            id:4,
+            name:'Roshmat',
+            status:'Barber',
+          },
+        ]
+      }
+    };
+  }
+  
+  
+
+
+  render() {
+
+  const  increase=() => {
+      this.setState({count:this.state.count+1});
+    };
+
+    const  decrease=() => {
+      this.setState({count:this.state.count-1});
+    };
+    const  reset=() => {
+      this.setState({count:0});
+    };
+    const onChange = (e) => {
+      this.setState({name: e.target.value})
+    };
+    const onDelete = (e)=>{
+      let res = this.state.data.dataList.filter((value)=>value.id !== e);
+      this.setState({
+        data:{
+          ...this.state.data,
+          dataList:res,
+        },
+      });
+    };
+    
+    return (
+      <>
+          <h1>Count  {this.state.count}</h1>   
+          <button onClick={increase}>+</button>   
+          <button onClick={decrease}>-</button>
+          <button onClick={reset}>reset</button>
+          <h1>name {this.state.name} </h1> 
+          <br />  
+          <input type="text" onChange={onChange}/>
+          
+          {
+          !this.state.data.dataList.length ?(
+            <h1>no data</h1>
+          ):(
+          this.state.data.dataList.map((value)=>(
+            <div key={value.id}>
+              <h1>
+                {value.id }
+                {value.name}
+                {value.status} <button onClick={()=>onDelete(value.id)}>delete</button>
+              </h1>
+            </div>
+            )))
+          }
+      </>
     );
   }
 }
 
-export default Student;
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -2,48 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App.jsx';
-import Container from './Container';
+// import Container from './Container';
  
 ReactDOM.render(
   <React.StrictMode>
-    <Container>
-       <App>
-           <div> One might confuse elements with a more widely known concept of “components”. We will introduce components in the next section. Elements are what components are “made of”, and we encourage you to read this section before jumping ahead. airbnb One might confuse elements with a more widely known concept of “components”. We will introduce components in the next section. Elements are what components are “made of”, and we encourage you to read this section before jumping ahead.</div>
-       </App>
-    </Container>
-    <Container>
-       <App>
-           <div> One might confuse elements with a more widely known concept of “components”. We will introduce components in the next section. Elements are what components are “made of”, and we encourage you to read this section before jumping ahead. airbnb One might confuse elements with a more widely known concept of “components”. We will introduce components in the next section. Elements are what components are “made of”, and we encourage you to read this section before jumping ahead.</div>
-       </App>
-    </Container>
-    <Container>
-       <App>
-           <div> One might confuse elements with a more widely known concept of “components”. We will introduce components in the next section. Elements are what components are “made of”, and we encourage you to read this section before jumping ahead. airbnb One might confuse elements with a more widely known concept of “components”. We will introduce components in the next section. Elements are what components are “made of”, and we encourage you to read this section before jumping ahead.</div>
-       </App>
-    </Container>
-    <Container>
-       <App>
-           <div> One might confuse elements with a more widely known concept of “components”. We will introduce components in the next section. Elements are what components are “made of”, and we encourage you to read this section before jumping ahead. airbnb One might confuse elements with a more widely known concept of “components”. We will introduce components in the next section. Elements are what components are “made of”, and we encourage you to read this section before jumping ahead.</div>
-       </App>
-    </Container>
-    <Container>
-       <App>
-           <div> One might confuse elements with a more widely known concept of “components”. We will introduce components in the next section. Elements are what components are “made of”, and we encourage you to read this section before jumping ahead. airbnb One might confuse elements with a more widely known concept of “components”. We will introduce components in the next section. Elements are what components are “made of”, and we encourage you to read this section before jumping ahead.</div>
-       </App>
-    </Container>
-    <Container>
-       <App>
-           <div> One might confuse elements with a more widely known concept of “components”. We will introduce components in the next section. Elements are what components are “made of”, and we encourage you to read this section before jumping ahead. airbnb One might confuse elements with a more widely known concept of “components”. We will introduce components in the next section. Elements are what components are “made of”, and we encourage you to read this section before jumping ahead.</div>
-       </App>
-    </Container>
-
-    <App status='ghv'/>
-    <App>
-        <h1>hello earth</h1>
-        <h1>hello earth</h1>
-        <h1>hello earth</h1>
-        <h1>hello earth</h1>
-    </App>
+    <App />
 
    </React.StrictMode>,
   document.getElementById('root')
